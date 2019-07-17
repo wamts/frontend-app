c72d6f7ac9b677f79e6f566e0f96360f45f7d675
Dongxue Hao <mshaodongxue@gmail.com>
2019-07-17 11:06:16 -0400

    Router change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, Router } from "react-router-dom";
 
 import { SignIn, SignUp, ForgotPassword, Dashboard } from './pages';
 import './App.scss';
@@ -8,13 +8,13 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-        <Switch>
+        <Router>
           <Route path="/signin" component={SignIn}/>
           <Route path="/signup" component={SignUp}/>
           <Route path="/forgotpassword" component={ForgotPassword}/>
           <Route path="/dashboard" component={Dashboard}/>
           <Route render={ props => <Redirect to={{ pathname: '/signin', state: { from: props.location } }} /> } />
-        </Switch>
+        </Router>
     </div>
   );
 }
