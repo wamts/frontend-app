9e7dc49b193a98cca98d3ce0c119967293b9970f
User <allan.hy.yuan@gmail.com>
2019-07-21 18:35:47 -0400

    Forgot Password page: Integrated with API

--- src/pages/ForgotPassword/ForgotPassword.js
+++ src/pages/ForgotPassword/ForgotPassword.js
@@ -1,29 +1,68 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-
 import './ForgotPassword.scss';
 
-class ForgotPassword extends Component {
-  render(){
-    return <div className="signin-container">
-        <div >
-          <form >
-            <div className="form-group">
-              <input type="email" className="form-control" id="email" placeholder="Email"></input>
-            </div>
-            <div >
-              <button type="submit" name="signIn" className=" btn btn-info fogotpwd btn">Forgot Password</button>
-            </div>
-          </form>
-        </div>
-        <div className="signup-options-container">
-          <NavLink to="/signIn" className="signup-link">Sign In</NavLink>
-          <NavLink to="/signUp" className="forgot-password-link">Sign Up</NavLink>
+const SERVER_ADDRESS = 'http://178.128.233.31'
 
-        </div>
+class ForgotPassword extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         retMsg: '',
+         submitted: false
+      }
+      this.handleSubmit = this.handleSubmit.bind(this);
+   }
+   handleSubmit(event) {
+      event.preventDefault()
+
+      let email = this.refs.email.value
+
+      this.setState({ submitted: true })
+
+      fetch(SERVER_ADDRESS + '/frontend/reset_password', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({
+            email: email,
+         })
+      }).then(res => res.json()
+      ).then(json => {
+         if (json.code === "Reset successful") {
+            // Do more stuff
+            this.setState({ retMsg: json.code })
+         } else {
+            // Display Error message
+            // **** Expected response: json.code + ": " + json.error
+            // **** Actual response:   json.msg, json.err (err is usually just {})
+            this.setState({ retMsg: json.msg })
+            console.log(json.msg)
+         }
+      });
+   }
+   render() {
+      return <div className="signin-container">
+         <div >
+            <form onSubmit={this.handleSubmit}>
+               <div className="form-group">
+                  <input type="email" className="form-control" id="email" placeholder="Email" ref='email' ></input>
+                  {this.state.submitted && this.state.retMsg ?
+                     <div className={this.state.retMsg === 'Reset failed' ? 'error-msg' : ''}>{this.state.retMsg}</div> : ''}
+               </div>
+               <div >
+                  <button type="submit" name="signIn" className=" btn btn-info fogotpwd btn">Forgot Password</button>
+               </div>
+            </form>
+         </div>
+         <div className="signup-options-container">
+            <NavLink to="/signIn" className="signup-link">Sign In</NavLink>
+            <NavLink to="/signUp" className="forgot-password-link">Sign Up</NavLink>
+         </div>
       </div>
-  }
+   }
 }
 
 export default ForgotPassword;
